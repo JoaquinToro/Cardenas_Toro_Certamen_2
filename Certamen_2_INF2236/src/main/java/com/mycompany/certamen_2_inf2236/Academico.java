aa3aa4aeaea173272c566648f2ef571a8a5054de
aguinfo <149103243+aguinfo@users.noreply.github.com>
2023-11-17 15:04:42 -0300

    Create Academico.java

---
+++ Certamen_2_INF2236/src/main/java/com/mycompany/certamen_2_inf2236/Academico.java
@@ -0,0 +1,31 @@
+public class Academico extends Libro{
+    String descripcion;
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Academico(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Academico(String descripcion, int libroID, String titulo, String autor, float precio) {
+        super(libroID, titulo, autor, precio);
+        this.descripcion = descripcion;
+    }
+
+    public Academico() {
+    }
+    public void leerDatos()
+    {
+        System.out.println("Titulo" + this.getTitulo());
+        System.out.println("Autor" + this.getAutor());
+        System.out.println("Precio" + this.getPrecio());
+        System.out.println("Descripcion" + this.descripcion);
+    }
+
+}
