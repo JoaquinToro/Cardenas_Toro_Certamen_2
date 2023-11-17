785cdeeb037cb38bb8838861718a221f740fc4a2
JoaquinToro <136033327+JoaquinToro@users.noreply.github.com>
2023-11-17 00:51:26 -0300

    Add files via upload

---
+++ Certamen_2_INF2236/src/main/java/com/mycompany/certamen_2_inf2236/Prueba.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.certamen_2_inf2236;
+
+/**
+ *
+ * @author Joaquin
+ */
+public class Prueba {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
